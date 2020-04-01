508fb477863fec2082e9eafad2447c125f798288
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-04-01 18:45:39 +0100

    Setup .yml file for project build (CI/CD).

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
 import React, { FC } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import ProtectedRoute from './components/ProtectedRoute';
-import Home from './components/Home';
-import Login from './components/Login';
-import { AppProps } from './actions';
-
-const App: FC = (props: AppProps) => {
-  const { isAuthenticated, isVerifying } = props;
-
+const App: FC = () => {
   return (
-    <Switch>
-      <ProtectedRoute
-        exact
-        path='/'
-        component={Home}
-        isAuthenticated={isAuthenticated}
-        isVerifying={isVerifying}
-      />
-      <Route path='/login' component={Login} />
-    </Switch>
+    <h1>Hello, Teach Me App!</h1>
   );
-};
+}
 
-const mapStateToProps = (state: AppProps) => {
-  return {
-    isAuthenticated: state.auth!.isAuthenticated,
-    isVerifying: state.auth!.isVerifying
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/styles/home.css.ts
+++
@@ -1,3 +0,0 @@
-export function styles(): any {
-  return {};
-}
