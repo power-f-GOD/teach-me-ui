0f9f5a1a6de84832f10187e1924a4b582cabddd1
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-04-09 23:36:54 +0100

    Connect firebase to app; create partial signup flow; fix bugs.

--- teach-me-ui/src/functions/index.ts
+++ teach-me-ui/src/functions/index.ts
@@ -1,3 +1,3 @@
 export * from './signup';
 export * from './utils';
-export * from './initStore';
+export * from '../appStore';
--- teach-me-ui/src/styles/signup.css.ts
+++ teach-me-ui/src/styles/signup.css.ts
@@ -3,12 +3,20 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      background: 'inherit'
+      background: 'inherit',
     },
     flexBasisHalved: {
       flexGrow: 1,
-      minWidth: '45%'
+      minWidth: '45%',
+    },
+    statusFeedback: {
+      fontSize: '0.9em',
+      textAlign: 'center',
+      whiteSpace: 'pre-wrap',
+      fontWeight: 'bold',
+      width: '100%'
     }
+    
   })
 );
 
