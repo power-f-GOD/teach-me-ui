792b890d0b097d05eaca16dd0ffc5b6cecf0dec6
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-04-25 18:25:21 +0100

    Add splash screen for landing page.

--- teach-me-ui/src/components/Landing.tsx
+++ teach-me-ui/src/components/Landing.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const Landing = () => {
   return (
-    <div className='fade-in'>
-      <br />
-      <br />
-      <br />
-      <h1>LANDING component goes here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-      <h1>More content goes here!</h1>
-      <h1>And more in here!</h1>
-    </div>
+    <Box className='fade-in'>
+      <Box component='main' className='landing-main-box'>
+        <Box component='section' className='landing-splash-box'>
+          <Container>
+            <Grid container direction='column'>
+              <Box maxWidth='35rem'>
+                <Typography component='h2' variant='h3'>
+                  Welcome to Teach Me!
+                </Typography>
+                <br />
+                <Typography component='span' variant='h6'>
+                  An online Community where Students, Lecturers, Academics,
+                  Universities converge!
+                </Typography>
+              </Box>
+              <br />
+              <br />
+              <Box className='landing-splash-links-box'>
+                <Link to='/signup'>JOIN NOW</Link>
+                <Link to='/about'>LEARN MORE</Link>
+              </Box>
+            </Grid>
+          </Container>
+        </Box>
+
+        <Box component='section'>
+          <Container>Some Extra content below!</Container>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
