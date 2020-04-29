954dbcb8afd9e09d0088f56d0fe2f0d8fccadfe1
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-04-29 22:32:35 +0100

    Add Landing page body.

--- teach-me-ui/src/components/Landing.tsx
+++ teach-me-ui/src/components/Landing.tsx
@@ -10,6 +10,14 @@ import LibraryBooksRoundedIcon from '@material-ui/icons/LibraryBooksRounded';
 import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import ArrowForward from '@material-ui/icons/ArrowForwardIosSharp';
+
+const tempText = `Mauris neque quam, fermentum ut nisl vitae, convallis maximus
+nisl. Sed mattis nunc id lorem euismod placerat. Vivamus
+porttitor magna enim, ac accumsan tortor cursus at. Phasellus
+sed ultricies mi non congue ullam corper. Praesent tincidunt
+sed tellus ut rutrum. Sed vitae justo condimentum, porta
+lectus vitae, ultricies congue gravida diam non fringilla.`;
 
 let mockData: any = {
   forCard: [
@@ -36,32 +44,17 @@ let mockData: any = {
     {
       key: 1,
       heading: 'Vivamus',
-      text: `Mauris neque quam, fermentum ut nisl vitae, convallis maximus
-      nisl. Sed mattis nunc id lorem euismod placerat. Vivamus
-      porttitor magna enim, ac accumsan tortor cursus at. Phasellus
-      sed ultricies mi non congue ullam corper. Praesent tincidunt
-      sed tellus ut rutrum. Sed vitae justo condimentum, porta
-      lectus vitae, ultricies congue gravida diam non fringilla.`
+      text: tempText
     },
     {
       key: 2,
       heading: 'Corper',
-      text: `Mauris neque quam, fermentum ut nisl vitae, convallis maximus
-      nisl. Sed mattis nunc id lorem euismod placerat. Vivamus
-      porttitor magna enim, ac accumsan tortor cursus at. Phasellus
-      sed ultricies mi non congue ullam corper. Praesent tincidunt
-      sed tellus ut rutrum. Sed vitae justo condimentum, porta
-      lectus vitae, ultricies congue gravida diam non fringilla.`
+      text: tempText
     },
     {
       key: 3,
       heading: 'Justo',
-      text: `Mauris neque quam, fermentum ut nisl vitae, convallis maximus
-      nisl. Sed mattis nunc id lorem euismod placerat. Vivamus
-      porttitor magna enim, ac accumsan tortor cursus at. Phasellus
-      sed ultricies mi non congue ullam corper. Praesent tincidunt
-      sed tellus ut rutrum. Sed vitae justo condimentum, porta
-      lectus vitae, ultricies congue gravida diam non fringilla.`
+      text: tempText
     }
   ]
 };
@@ -87,7 +80,7 @@ const Landing = () => {
               <br />
               <Box className='landing-splash-links-box'>
                 <Link to='/signup' className='Primary'>
-                  JOIN NOW
+                  JOIN NOW <ArrowForward fontSize='inherit' />
                 </Link>
                 <Link to='/about'>LEARN MORE</Link>
               </Box>
@@ -103,33 +96,55 @@ const Landing = () => {
               ))}
             </Container>
           </Box>
-          <Box className='text-section-row text-section-1'>
+          {mockData.forClipArt.map((item: any, key: number) => (
+            <ClipArtWithText item={item} key={key} />
+          ))}
+          <Box className='text-section-row text-section-last'>
             <Container className='text-section-container'>
               <Box>
                 <Typography component='h3' variant='h4'>
                   Fermentum
                 </Typography>
                 <hr />
-                <Box component='span'>
-                  Mauris neque quam, fermentum ut nisl vitae, convallis maximus
-                  nisl. Sed mattis nunc id lorem euismod placerat. Vivamus
-                  porttitor magna enim, ac accumsan tortor cursus at. Phasellus
-                  sed ultricies mi non congue ullam corper. Praesent tincidunt
-                  sed tellus ut rutrum. Sed vitae justo condimentum, porta
-                  lectus vitae, ultricies congue gravida diam non fringilla.
-                </Box>
+                <Box component='span'>{mockData.forClipArt[0].text}</Box>
               </Box>
             </Container>
           </Box>
-          {mockData.forClipArt.map((item: any, key: number) => (
-            <ClipArtWithText item={item} key={key} />
-          ))}
-          <Box className='text-section-row photos'>
+          <Box className='text-section-row avatars-box'>
+            <Container className='text-section-container avatars-box-header-container'>
+              <Box>
+                <Typography
+                  className='avatars-box-header'
+                  component='h3'
+                  variant='h4'>
+                  Erectus
+                </Typography>
+              </Box>
+            </Container>
             <Container className='text-section-container'>
               <Box className='text-section-box'>
+                <Box className='avatar-medium'></Box>
+                <Box className='avatar-small'></Box>
               </Box>
               <Box className='text-section-box'>
+                <Box className='avatar-small'></Box>
+                <Box className='avatar-medium'></Box>
               </Box>
+            </Container>
+          </Box>
+          <Box className='card-section-row card-section-last-box'>
+            <Container className='card-section-container'>
+              {mockData.forCard.map((item: any, key: number) =>
+                key > 0 ? (
+                  <CardComponent item={item} key={key} className='card-below' />
+                ) : null
+              )}
+            </Container>
+          </Box>
+          <Box className='text-section-row'>
+            <Container className='text-section-container'>
+              <Box className='text-section-box'></Box>
+              <Box className='text-section-box'></Box>
             </Container>
           </Box>
         </Box>
@@ -139,21 +154,25 @@ const Landing = () => {
 };
 
 function CardComponent(props: any) {
-  let { icon: Icon, heading, text } = props.item;
+  const { icon: Icon, heading, text } = props.item;
+  const { className } = props;
 
   return (
-    <Card>
+    <Card className={className ?? ''}>
       <CardContent>
         <Box className='icon-box'>
           <Icon className='icon-large' />
         </Box>
-        <Box>
+        <Box className='card-header-box'>
           <Typography component='h3' variant='h4'>
             {heading}
           </Typography>
           <hr />
         </Box>
-        {text}
+        <Box>
+          {text}
+          {/card-below/.test(className) && <Box className='card-link-box'><Link to='/#!' className='outlined'>Link</Link></Box>}
+        </Box>
       </CardContent>
     </Card>
   );
