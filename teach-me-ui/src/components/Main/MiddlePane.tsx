a8f54e78ac190bcb455e0dbe359c940bb09dd312
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-05-25 20:17:09 +0100

    Create basic homepage sections blueprint; add chat button.

---
+++ teach-me-ui/src/components/Main/MiddlePane.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// import Box from '@material-ui/core/Box';
+
+const MiddlePane = () => {
+  return (
+    <Container className='middle-pane p-0'>
+      
+    </Container>
+  );
+};
+
+export default MiddlePane;
---
+++ teach-me-ui/src/components/Main/RightPane.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// import Box from '@material-ui/core/Box';
+
+const RightPane = () => {
+  return (
+    <Container className='right-pane p-0'>
+      
+    </Container>
+  );
+};
+
+export default RightPane;
