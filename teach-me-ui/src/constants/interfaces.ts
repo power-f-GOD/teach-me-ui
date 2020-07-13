b543163fb3690fb0a4bcd739c5ac63aef2473c0c
Sunday Power Inemesit <sundaypower9@gmail.com>
2020-07-14 00:06:15 +0100

    Add message scheduled and message sent receipts; Modify Styles file/folder structure; Clean up.

--- teach-me-ui/src/constants/interfaces.ts
+++ teach-me-ui/src/constants/interfaces.ts
@@ -225,7 +225,7 @@ export interface CreateLevelState extends StatusPropsState {
 // ChatBox interfaces...
 
 //you should eventually make all the Message props required
-export interface Message extends Partial<APIMessageResponse> {
+export interface MessageProps extends Partial<APIMessageResponse> {
   timestamp?: string | number;
 }
 
@@ -242,7 +242,7 @@ export interface ChatData {
 export interface AnchorInfo {
   displayName: string;
   id: string;
-  messages?: Message[];
+  messages?: MessageProps[];
   avatar: string;
   info?: UserInfo | RoomInfo;
   type: 'conversation' | 'classroom';
