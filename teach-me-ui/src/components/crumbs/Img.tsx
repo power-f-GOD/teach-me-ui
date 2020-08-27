8d97c3d457a633f8193bf9771027183b7a96062d
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-08-27 20:19:56 +0100

    fixed image error for undefined

--- teach-me-ui/src/components/crumbs/Img.tsx
+++ teach-me-ui/src/components/crumbs/Img.tsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 export default (props: any) => {
   const [error, setError] = useState(false);
   const onError = (e: any) => {
+    console.log('image error occurred');
     setError(true);
   };
   return (
     <img
       alt='Description goes here'
       {...props}
+      src={props.src || 'undefined'}
       style={{ display: error ? 'none' : 'block' }}
       onError={onError}
     />
