c488cfd3b1edebe02abf1c4bca2e80fd4fc7c4de
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-08-28 09:59:30 +0100

    fixed compose component failing

--- teach-me-ui/src/__tests__/components/Compose.test.tsx
+++ teach-me-ui/src/__tests__/components/Compose.test.tsx
@@ -4,10 +4,14 @@ import React from 'react';
 // import react-testing methods
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 
+import { connect } from 'react-redux';
+
 // add custom jest matchers from jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
-import Compose from '../../components/crumbs/Compose';
+import ComposeBase from '../../components/crumbs/Compose';
+
+const Compose = connect(({ userData }: any) => ({ userData }))(ComposeBase);
 
 test('loads and displays placeholder text', async () => {
   render(<Compose />);
