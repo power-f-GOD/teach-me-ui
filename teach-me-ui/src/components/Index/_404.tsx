aa7a0c2d42f740ab6ee3fe7b5085b5e817db8e0e
power-f-GOD <sundaypower9@gmail.com>
2020-09-02 21:19:56 +0100

    Fix container fluidity ish in Auth and _404 components caused by initial modifications.

--- teach-me-ui/src/components/Index/_404.tsx
+++ teach-me-ui/src/components/Index/_404.tsx
@@ -8,7 +8,7 @@ const _404 = () => {
   return (
     <Box
       component='main'
-      className='root-grid container-404 flex-column d-flex justify-content-center align-items-center fade-in'
+      className='root-grid flex-column d-flex justify-content-center align-items-center fade-in'
       paddingY='100px'>
       <Box component='section'>
         <svg
